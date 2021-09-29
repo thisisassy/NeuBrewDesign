c86bb542892e6497b99b5ae66f7b085894a9cb2c
Haruka Ashiuchi <chopperman1224@gmail.com>
2021-09-29 16:57:19 +0900

    プロフィールを追加

---
+++ src/components/Author/author.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Link } from "gatsby"
+import * as styles from "../Author/index.module.scss"
+import Profile from "../../images/profile.jpg"
+
+const Author = () => {
+  return (
+    <div className={styles.authorItem}>
+      <h3 className={styles.heading}>この記事を書いた人</h3>
+      <div className={styles.authorBody}>
+        <img src={Profile} alt="" />
+        <div>
+          <h4>Haruka</h4>
+          <p>デザインとコーディングのメモ。生活と読書、思考の記録を書いています。</p>
+          <p><Link to={`/about/`}>詳しいプロフィールをみる</Link></p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Author
