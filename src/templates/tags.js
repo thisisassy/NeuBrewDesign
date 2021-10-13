f587177dfcd3be63cbc774536bec5274894b5cba
Haruka Ashiuchi <chopperman1224@gmail.com>
2021-10-13 16:39:34 +0900

    tag一覧ページを作成

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import Emoji from "react-emoji-render"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const Tags = ({ data, pageContext }) => {
   const { tag } = pageContext
-  const { totalCount } = data.allMarkdownRemark
+  const { edges, totalCount } = data.allMarkdownRemark
   const tagHeader = `${tag}の記事一覧`
 
   return (
@@ -13,11 +14,24 @@ const Tags = ({ data, pageContext }) => {
       <Seo pagetitle={tagHeader} />
       <section>
         <h1>{tagHeader}<span>{totalCount}</span></h1>
-        <div>
-          <article>
-            <Link to={`/`}>
-            </Link>
-          </article>
+        <div className={`tagEntry`}>
+          {edges.map(({ node }) => (
+            <article className={`tagEntryItem`} key={node.frontmatter.slug}>
+              <Link to={`/${node.frontmatter.slug}`}>
+                <figure className={`entry__thumbnail`}>
+                  <Emoji text={node.frontmatter.emoji || "📝"} />
+                </figure>
+                <div className={`entryHeader`}>
+                  <h2>{node.frontmatter.title}</h2>
+                  <p>{node.excerpt}</p>
+                  <div className={`entryMeta`}>
+                    <p><time dateTime={node.frontmatter.date}>{node.frontmatter.date}</time></p>
+                    <p>{node.frontmatter.category}</p>
+                  </div>
+                </div>
+              </Link>
+            </article>
+          ))}
         </div>
       </section>
     </Layout>
@@ -25,16 +39,22 @@ const Tags = ({ data, pageContext }) => {
 }
 
 export const query = graphql`
-query {
-  allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
-    group(field: frontmatter___tags) {
-      fieldValue
-      totalCount
-      field
-    }
-    nodes {
-      frontmatter {
-        title
+query($tag: String)  {
+  allMarkdownRemark(
+    sort: {fields: frontmatter___date, order: DESC}
+    filter: {frontmatter: {tags: {in: [$tag]}}}
+  ) {
+    edges {
+      node {
+        frontmatter {
+          title
+          slug
+          date(formatString: "MMM D YYYY")
+          draft
+          emoji
+          thumbnail
+          tags
+        }
       }
     }
   }
