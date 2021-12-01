8fbdac273f38da48c0b30a2ea227814d3b35b234
Haruka Ashiuchi <chopperman1224@gmail.com>
2021-12-01 17:43:53 +0900

    allPostsをコンポーネントにした

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
-import Emoji from "react-emoji-render"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../styles/pages/index.module.scss"
-
+import AllPosts from "../components/AllPosts/allposts"
 
 const IndexPage = ({ data }) => {
-  const RecentEntry = data.recent.edges
   const category = data.cat.group
   const sum = data.cat.totalCount
   return (
@@ -22,60 +20,13 @@ const IndexPage = ({ data }) => {
             ))}
           </ul>
         </div>
-        <div className={styles.NewestEntry}>
-          {RecentEntry.map(({ node }) => (
-            <article className={`recentItem`} key={node.frontmatter.slug}>
-              <Link to={`/${node.frontmatter.slug}`}>
-                <figure className={styles.entry__thumbnail}>
-                  <Emoji text={node.frontmatter.emoji || "📝"} />
-                </figure>
-                <div className={styles.entryHeader}>
-                  <h2>{node.frontmatter.title}</h2>
-                  <p>{node.excerpt}</p>
-                  <div className={styles.entryMeta}>
-                    <p><time dateTime={node.frontmatter.date}>{node.frontmatter.date}</time></p>
-                    <p>{node.frontmatter.category}</p>
-                  </div>
-                </div>
-              </Link>
-            </article>
-          ))}
-        </div>
       </section>
-      <p className={styles.seeMoreLink}><Link to={`/allposts/`}>See more posts</Link></p>
+      <AllPosts />
     </Layout>
   )
 }
 export const query = graphql`
 {
-  recent: allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}, limit: 12)  {
-    edges {
-      node {
-        frontmatter {
-          title
-          date(formatString: "MMM D YYYY")
-          category
-          slug
-          emoji
-        }
-        excerpt(format: PLAIN, pruneLength: 50, truncate: true)
-      }
-    }
-  }
-  entry: allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}, skip: 3, limit: 7) {
-    edges {
-      node {
-        frontmatter {
-          title
-          date(formatString: "MMM D YYYY")
-          category
-          slug
-          emoji
-        }
-        excerpt(format: PLAIN, pruneLength: 80, truncate: true)
-      }
-    }
-  }
   cat: allMarkdownRemark {
     totalCount
     group(field: frontmatter___category) {
