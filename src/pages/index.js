800181d70e440b7682ea30c947287fb5921d70a3
Haruka Ashiuchi <chopperman1224@gmail.com>
2021-12-01 17:19:10 +0900

    stylesを変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { graphql, Link } from "gatsby"
 import Emoji from "react-emoji-render"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import * as styles from "../styles/pages/index.module.scss"
 
 
 const IndexPage = ({ data }) => {
@@ -13,25 +14,25 @@ const IndexPage = ({ data }) => {
     <Layout>
       <Seo />
       <section>
-        <div className={`sectionHead`}>
+        <div className={styles.sectionHead}>
           <h3>All {sum} posts</h3>
-          <ul className={`catList`}>
+          <ul className={styles.catList}>
             {category.map(category => (
               <li><Link to={`/categories/${category.fieldValue}/`}>{category.fieldValue}</Link></li>
             ))}
           </ul>
         </div>
-        <div className={`NewestEntry`}>
+        <div className={styles.NewestEntry}>
           {RecentEntry.map(({ node }) => (
             <article className={`recentItem`} key={node.frontmatter.slug}>
               <Link to={`/${node.frontmatter.slug}`}>
-                <figure className={`entry__thumbnail`}>
+                <figure className={styles.entry__thumbnail}>
                   <Emoji text={node.frontmatter.emoji || "📝"} />
                 </figure>
-                <div className={`entryHeader`}>
+                <div className={styles.entryHeader}>
                   <h2>{node.frontmatter.title}</h2>
                   <p>{node.excerpt}</p>
-                  <div className={`entryMeta`}>
+                  <div className={styles.entryMeta}>
                     <p><time dateTime={node.frontmatter.date}>{node.frontmatter.date}</time></p>
                     <p>{node.frontmatter.category}</p>
                   </div>
@@ -41,13 +42,13 @@ const IndexPage = ({ data }) => {
           ))}
         </div>
       </section>
-      <p className={`seeMoreLink`}><Link to={`/allposts/`}>See more posts</Link></p>
+      <p className={styles.seeMoreLink}><Link to={`/allposts/`}>See more posts</Link></p>
     </Layout>
   )
 }
 export const query = graphql`
 {
-  recent: allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}, limit: 6)  {
+  recent: allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}, limit: 12)  {
     edges {
       node {
         frontmatter {
